5d45e6b9c30218b712a9b91b620dfe53e895b31f
damilola <ajay0006@algonquinlive.com>
2023-11-02 11:48:22 -0400

    modified code to allow editing of player names and split into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./Components/Player/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player symbol='X' name='Player 1'/>
+                    <Player symbol='O' name='Player 2'/>
+                </ol>
+            </div>
+        </main>
+    )
 }
 
 export default App
---
+++ src/Components/Player/Player.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(name)
+    let playerNameElement = <span className='player-name'>{playerName}</span>
+
+
+    function onClickEdit() {
+        setIsEditing(prevState => !prevState)
+        // setIsEditing(!isEditing)
+    }
+
+    function onPlayerNameChange(e) {
+        setPlayerName(e.target.value)
+    }
+
+    if (isEditing) {
+        playerNameElement = <input type='text' required value={playerName} onChange={onPlayerNameChange}/>
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {playerNameElement}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={onClickEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
