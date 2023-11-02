78c03ffee71e6da807e809d02ca706d04deac7f9
damilola <ajay0006@algonquinlive.com>
2023-11-02 16:05:56 -0400

    configured the board component to accept values

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -4,15 +4,28 @@ const gameBoardDefault = Array(3).fill(new Array(3).fill(null))
 
 export default function GameBoard() {
     const [gameBoard, setGameBoard] = useState(gameBoardDefault)
+
+    function onClickGameSquare(rowIndex, colIndex) {
+        setGameBoard(prevState => {
+            // I am deep copying the previous state array, now because it is a matrix,
+            // I am using the spread operator twice
+            // once for the whole array, and another time for the individual array using the map feature
+            const prevGameBoard = [...prevState.map(innerArray => [...innerArray])]
+            // update the array where the square was clicked, which matches with the index on the array
+            prevGameBoard[rowIndex][colIndex] = "X"
+
+            return prevGameBoard
+        })
+    }
     return (
         <ol id='game-board'>
-            {gameBoardDefault.map
+            {gameBoard.map
                 ((row, rowIndex) =>
                         <li key={rowIndex}>
                             <ol>
                                 {row.map((colSymbol, colIndex) =>
                                     <li key={colIndex}>
-                                        <button>
+                                        <button onClick={() => onClickGameSquare(rowIndex, colIndex)}>
                                             {colSymbol}
                                         </button>
                                     </li>
